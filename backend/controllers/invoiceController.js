691dfff2296450a0572725cbf3dd0b8277d743c8
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-07-15 14:08:16 +0530

    added addinvoice controller

--- backend/controllers/invoiceController.js
+++ backend/controllers/invoiceController.js
@@ -1,14 +1,39 @@
 // controllers/invoiceController.js
 
+import Customer from '../models/customer.model.js';
 import Invoice from '../models/invoice.js';
 
-export const createInvoice = async (req, res) => {
+export const addInvoice = async (req, res) => {
   try {
-    const invoice = new Invoice(req.body);
-    await invoice.save();
-    res.status(201).json(invoice);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+    const { name, amount, invoiceDate } = req.body;
+    const customerName = name.trim();
+
+  // find if the user ezits or not
+  let customer = await Customer.findOne({ name: customerName });
+  if (!customer) {
+    // if not, create a new customer
+    customer = await Customer.create({ name: customerName });
+  }
+
+  // here we create the invoice
+  const invoice = await Invoice.create({
+    customer: customer._id,
+    amount,
+    date: invoiceDate,
+  });
+
+  res.status(201).json({
+    message: 'Invoice created successfully',
+    invoice: {
+      id: invoice._id,
+      customer: customer.name,
+      amount: invoice.amount,
+      date: invoice.date,
+    },
+  });
+  } catch (error) {
+    console.error("❌ Error in addInvoice:", error.message);
+    res.status(500).json({ message: error.message });
   }
 };
 

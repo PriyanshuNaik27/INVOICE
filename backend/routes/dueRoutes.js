95827b5d0506d1f80edd8459b4d0acff55adc7d6
ABHISHEK KUMAR JHA <abhishekjhaji2004@gmail.com>
2025-07-16 19:33:07 +0530

    chnages

--- backend/routes/dueRoutes.js
+++ backend/routes/dueRoutes.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { getCustomerDue } from "../controllers/dueController.js";
+// import express from "express";
+// import { getCustomerDue } from "../controllers/dueController.js";
 
-const router = express.Router();
-router.get("/:customerName", getCustomerDue);
+// const router = express.Router();
+// router.get("/:customerName", getCustomerDue);
 
-export default router;
+// export default router;
 
 

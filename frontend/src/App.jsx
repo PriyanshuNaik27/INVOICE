b04179154fdc61ff22f80aa75a94a4359f1dfd19
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-07-18 00:54:38 +0530

    made chnages for dep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Nav";
 import ChatBox from "./components/ChatBox";
 import Team from "./components/Team";
 
